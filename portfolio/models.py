470c2d47ad0d2401b1a7a4c92599a66c1812733c
Miraculos-bmk <miraclebenjamin050@gmail.com>
2024-05-10 02:44:40 -1100

    No3 commit

--- portfolio/models.py
+++ portfolio/models.py
@@ -7,3 +7,16 @@ class Contact(models.Model):
     email=models.EmailField()
     phone_number= models.CharField(max_length=10)
     description=models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class Blogs(models.Model):
+    title=models.CharField(max_length=60)
+    description=models.TimeField()
+    authname=models.CharField(max_length=15)
+    img=models.ImageField(upload_to='blog',blank=True, null=True)
+    timeStamp=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from portfolio.models import Contact
 
 # Create your views here.
 def home(request):
@@ -6,6 +8,17 @@ def home(request):
 def about(request):
     return render(request, "about.html")
 def contact(request):
+    if request.method=="POST":
+        f_name=request.POST.get('name')
+        f_email=request.POST.get('email')
+        f_phone=request.POST.get('num')
+        f_desc=request.POST.get('desc')
+        query=Contact(name=f_name,email=f_email,phone_number=f_phone,description=f_desc)
+        query.save()
+        messages.success(request, "Thanks for contacting us. We will get back to you soon!")
+
+        return redirect('/contact')
     return render(request, "contact.html")
+    
 def services(request):
     return render(request, "services.html")
